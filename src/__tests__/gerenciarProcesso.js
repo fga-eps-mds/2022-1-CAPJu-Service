9d52d27a7e52c6db5a50b97ac6d6fdf4c8c17a22
Arthur Tavares <arthur001123@gmail.com>
2022-07-29 18:34:20 -0300

    Configurando geração de arquivos de release

--- src/__tests__/gerenciarProcesso.js
+++ src/__tests__/gerenciarProcesso.js
@@ -6,34 +6,33 @@ jest.mock("mongoose", () => {
   return { connect: async () => undefined };
 });
 jest.mock("../schemas/Process.js", () => {
-    return {
-        create: (json) => json,
-        find: () => "foobar"
-    }
+  return {
+    create: (json) => json,
+    find: () => "foobar",
+  };
 });
 test("testa o endpoint newProcess", async () => {
-    const response = await supertest(app)
-        .post("/newProcess")
-        .set("Content-Type", "application/json")
-        .send({
-            registro: "foo",
-            apelido: "bar"
-        });
-    expect(response.status).toBe(200)
-    expect(response.body).toEqual({
-        registro: "foo",
-        apelido: "bar",
-        ...response.body
+  const response = await supertest(app)
+    .post("/newProcess")
+    .set("Content-Type", "application/json")
+    .send({
+      registro: "foo",
+      apelido: "bar",
     });
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual({
+    registro: "foo",
+    apelido: "bar",
+    ...response.body,
+  });
 });
 
 test("testa o endpoint newProcess se der errado", async () => {
-    const response = await supertest(app)
-        .post("/newProcess")
-        .set("Content-Type", "application/json")
-        .send({
-            apelido: "bar"
-        });
-    expect(response.status).toBe(500);
-    
+  const response = await supertest(app)
+    .post("/newProcess")
+    .set("Content-Type", "application/json")
+    .send({
+      apelido: "bar",
+    });
+  expect(response.status).toBe(500);
 });
