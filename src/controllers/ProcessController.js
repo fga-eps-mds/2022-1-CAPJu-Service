c8647e57c94071ffc19eb1ab083f3e10c435aaca
MaiconMares <maiconmaresunb@gmail.com>
2022-07-31 20:36:42 -0300

    Criando rota para atualizar processo.

--- src/controllers/ProcessController.js
+++ src/controllers/ProcessController.js
@@ -4,12 +4,16 @@ import { ProcessValidator } from "../validators/Process.js";
 class ProcessController {
   async createProcess(req, res) {
     try {
-      const { registro, apelido } = await ProcessValidator.validateAsync(
+      const { registro, apelido, etapaAtual, etapas, arquivado, fluxoId } = await ProcessValidator.validateAsync(
         req.body
       );
       const process = await Process.create({
         registro,
         apelido,
+        etapaAtual, 
+        arquivado,
+        etapas,
+        fluxoId
       });
 
       return res.status(200).json(process);
@@ -26,6 +30,21 @@ class ProcessController {
       return res.status(200).json({
         processes,
       });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
+    }
+  }
+
+  async updateProcess(req, res) {
+    try {
+      const { registro, apelido, etapaAtual, etapas, arquivado, fluxoId } = await ProcessValidator.validateAsync(
+        req.body
+      );
+
+      const process = await Process.findOneAndUpdate( req.params.id, req.body, {new: true});
+      
+      return res.status(200).json(process);
     } catch (error) {
       console.log(error);
       return res.status(500).json(error);
