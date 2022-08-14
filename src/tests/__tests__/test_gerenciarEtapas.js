d5365249ecc6348306037471abfd9dd86ffe08ca
MaiconMares <maiconmaresunb@gmail.com>
2022-08-13 23:49:01 -0300

    Corrigindo erro de conexao ao bd e implementando teste de remocao em etapas.
    Co-authored-by: lucascaldasb <lucas20caldas@gmail.com>
    Co-authored-by: GabrielGaba <gabriel-ara-sou@hotmail.com>

--- src/tests/__tests__/test_gerenciarEtapas.js
+++ src/tests/__tests__/test_gerenciarEtapas.js
@@ -5,37 +5,48 @@ import app from "../../app";
 let globalResponse;
 
 beforeAll(async () => {
-    mongoDB.connect();
-    await mongoDB.mongoose.connection.dropDatabase();
-    globalResponse = await supertest(app)
-      .post("/newStage")
-      .set("Content-Type", "application/json")
-      .send({
-        name: "name",
-        time:"15",
-        deleted: false,
-      });
-  }, 60000);
-afterAll((done) => {
-    mongoDB.disconnect(done);
-});
-describe("post new stage", () => {
-    test("testa o endpoint newStage", async () => {
-        expect(globalResponse.status).toBe(200);
-        expect(globalResponse.body).toEqual({
-          name: "teste2Stage",
-          time: "22",
-          ...globalResponse.body,
-        });
-      });
-    test("testa o endpoint newStage se der errado", async () => {
-        const response = await supertest(app)
-        .post("/newStage")
-        .set("Content-Type", "application/json")
-        .send({
-            name:"",
-            time:"",
-        });
-        expect(response.status).toBe(500);
+  mongoDB.connect();
+  await mongoDB.mongoose.connection.dropDatabase();
+
+  globalResponse = await supertest(app)
+    .post("/newStage")
+    .send({
+      name: "name",
+      time: "15",
+      deleted: false,
     });
 });
+
+console.log('ok1');
+
+afterAll((done) => {
+  mongoDB.disconnect(done);
+});
+
+test("testa o endpoint newStage", async () => {
+  expect(globalResponse.status).toBe(200);
+  expect(globalResponse.body).toEqual({
+    name: "teste2Stage",
+    time: "22",
+    ...globalResponse.body,
+  });
+});
+
+test("testa o endpoint newStage se der errado", async () => {
+  const response = await supertest(app)
+    .post("/newStage")
+    .send({
+      name: "",
+      time: "",
+    });
+  expect(response.status).toBe(500);
+});
+
+test("testa a remoção de uma etapa em /deleteStage", async () => {
+  const response = await supertest(app)
+  .post("/deleteStage")
+  .send({
+    stageId: globalResponse.body._id
+  });
+  expect(response.status).toBe(200);
+})
