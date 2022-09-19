fe20d5ef332f46d746723befcb3b951cff69f7a5
Arthur Tavares <arthur001123@gmail.com>
2022-09-19 00:03:32 -0300

    Consertando middleware authRole nos testes

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -4,6 +4,9 @@ dotenv.config();
 jest.mock("../middleware/authMiddleware", () => {
   return {
     protect: jest.fn((req, res, next) => next()),
+    authRole: jest.fn((roleArray) => (req, res, next) => {
+      next();
+    }),
   };
 });
 
